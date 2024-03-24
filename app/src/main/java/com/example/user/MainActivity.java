a791ebc7cb34cb7948a291c86f92361397809008
Abdallah Azzam <dev.azzam@gmail.com>
2024-03-24 13:22:00 -0400

    new test

--- app/src/main/java/com/example/user/MainActivity.java
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -14,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //this commen
+    //this comme
     Button btn_login, btn_signup;
     TextView forgot_pwd;
     CheckBox rememberMe;
