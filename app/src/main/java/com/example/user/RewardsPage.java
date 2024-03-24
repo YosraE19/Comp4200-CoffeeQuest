348113652144102a87321f9563eab7c304482e31
natalieminer <100392481+natalieminer@users.noreply.github.com>
2024-03-24 16:05:31 -0400

    Rewards page version 2 - updated to use MyDBHelper functions onCreate

--- app/src/main/java/com/example/user/RewardsPage.java
+++ app/src/main/java/com/example/user/RewardsPage.java
@@ -1,6 +1,8 @@
 package com.example.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -14,18 +16,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RewardsPage extends AppCompatActivity {
 
-    ImageView back_btn, home_btn;
-    TextView titleRewards, welcomeText, userText ;
+    ImageView home_btn;
+    TextView titleRewards, welcomeText, userText;
     FrameLayout frame;
     ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rewards_page);
 
+        // Enable edge-to-edge display
+        EdgeToEdge.enable(this);
 
-        back_btn = findViewById(R.id.backBtnRewards);
+        // Initialize views
         home_btn = findViewById(R.id.homeBtnRewards);
         titleRewards = findViewById(R.id.titleRewards);
         welcomeText = findViewById(R.id.welcomeText);
@@ -33,24 +37,33 @@ public class RewardsPage extends AppCompatActivity {
         frame = findViewById(R.id.frame);
         progressBar = findViewById(R.id.progressBar);
 
-        //Initialize DB
+        // Initialize DB
         MyDBHelper dbHelper = new MyDBHelper(getApplicationContext());
 
-        //get userNickname for welcome message
-        String userNickname = dbHelper.getUserNickname(1); // TODO to be modified to select logged in user _id
+        // Get userNickname for welcome message
+        String userNickname = dbHelper.getUserNickname(1); // TODO: Modify to select logged-in user _id
         userText.setText(userNickname);
 
-        //initialize progress bar with points for _idUser
-        int points = dbHelper.getPointsValue(1); // TODO to be modified to select logged in _idUser
+        // Initialize progress bar with points for _idUser
+        int points = dbHelper.getPointsValue(1); // TODO: Modify to select logged-in _idUser
         progressBar.setProgress(points);
 
+        // Apply window insets listener for edge-to-edge display
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Set onClickListener for the home button
+        home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open HomePage
+                Intent intent = new Intent(RewardsPage.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
-
-
-
 }
