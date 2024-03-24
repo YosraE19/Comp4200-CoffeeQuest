f6f76d84548021b24ff19c7f919b5c570d470189
Abdallah Azzam <dev.azzam@gmail.com>
2024-03-23 23:13:43 -0400

    testing push pull

--- app/src/main/java/com/example/user/MainActivity.java
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     TextView forgot_pwd;
     CheckBox rememberMe;
     EditText email,pwd;
-    //abdallah
+    //abdallah azzan
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
